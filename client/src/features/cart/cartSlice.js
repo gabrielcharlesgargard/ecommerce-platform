945671a449309e46faca43b60ae30815ed57c630
gabrielcharlesgargard <gabrielcharles368@gmail.com>
2026-04-30 07:49:48 +0530

    feat: Upgrade persist cart state(Reducers)

--- client/src/features/cart/cartSlice.js
+++ client/src/features/cart/cartSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const savedCart = localStorage.getItem("cartItems");
 const initialState = {
-  items: [],
+  items: savedCart ? JSON.parse(savedCart) : [],
 };
 
 const cartSlice = createSlice({
@@ -17,22 +18,28 @@ const cartSlice = createSlice({
       } else {
         state.items.push({ ...newItem, quantity: 1 });
       }
+
+      localStorage.setItem("cartItems", JSON.stringify(state.items));
     },
     removeFromCart: (state, action) => {
       state.items = state.items.filter((item) => item.id !== action.payload);
+      localStorage.setItem("cartItems", JSON.stringify(state.items));
     },
     increaseQuantity: (state, action) => {
       const item = state.items.find((item) => item.id === action.payload);
       if (item) item.quantity += 1;
+      localStorage.setItem("cartItems", JSON.stringify(state.items));
     },
     decreaseQuantity: (state, action) => {
       const item = state.items.find((item) => item.id === action.payload);
       if (item && item.quantity > 1) {
         item.quantity -= 1;
       }
+      localStorage.setItem("cartItems", JSON.stringify(state.items));
     },
     clearCart: (state) => {
       state.items = [];
+      localStorage.removeItem("cartItems");
     },
   },
 });
