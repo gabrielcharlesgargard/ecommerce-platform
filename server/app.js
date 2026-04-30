7c5a3032d14e69c961176eeadd3c1e9a88c99a25
gabrielcharlesgargard <gabrielcharles368@gmail.com>
2026-04-30 13:21:28 +0530

    feat: Update App.js

--- server/app.js
+++ server/app.js
@@ -1,19 +1,22 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
-import {notFound, errorHandler} from "./middleware/errorMiddleware.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
-app.use( cors() );
-app.use( express.json() );
-app.use( notFound );
-app.use( errorHandler );
-app.use( "/api/auth", authRoutes );
+app.use(cors());
+app.use(express.json());
 
+// Routes FIRST
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.get( "/", ( req, res ) => {
-    res.send( "API is running..." );
-} );
+app.use("/api/auth", authRoutes);
+
+// Error handlers LAST
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
