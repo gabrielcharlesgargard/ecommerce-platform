307e75d704feb2fef6ae862140e8e28026bdbf40
gabrielcharlesgargard <gabrielcharles368@gmail.com>
2026-04-30 10:50:43 +0530

    feat: Add a basic error handler

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import {notFound, errorHandler} from "./middleware/errorMiddleware.js";
 
 const app = express();
 
 app.use( cors() );
 app.use( express.json() );
+app.use( notFound );
+app.use( errorHandler );
 app.use( "/api/auth", authRoutes );
 
 
